6b3f288c4878ea52d8a9a9330015db1604878bf8
Bea Lam <bea.lam@jollamobile.com>
2016-11-27 09:37:50 +1000

    [bluez-qt] Fix licensing for patched files
    Use full LGPLv2.1 license header instead of "Alternative, this file
    may be used under the terms of [LGPL 2.1]..." since there is no
    alternative license provided.

--- bluez-qt/src/bluez4/manager_bluez4_p.h
+++ bluez-qt/src/bluez4/manager_bluez4_p.h
@@ -3,13 +3,19 @@
 ** Copyright (C) 2016 Jolla Ltd.
 ** Contact: Bea Lam <bea.lam@jolla.com>
 **
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** This file is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This file is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this file; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
 ****************************************************************************/
 
